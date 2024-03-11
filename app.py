2b5caaa87856e01d830314655393f8da18125aa1
peteciank <106826936+peteciank@users.noreply.github.com>
2024-03-11 16:26:00 -0300

    summarization plus

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ import streamlit as st
 from transformers import pipeline
 
 
-pipe = pipeline('sentiment-analysis')
+pipe_sent = pipeline('sentiment-analysis')
+pipe_summ = pipeline("summarization", model="facebook/bart-large-cnn")
+
 text = st.text_area('Enter the text to analyze')
 
 if text:
-    out = pipe(text)
+    st.write("Sentiment")
+    out = pipe_sent(text)
     st.json(out)
+
+    st.write("Summarization")
+    out_summ = pipe_summ(text)
+    st.json(out_summ)
